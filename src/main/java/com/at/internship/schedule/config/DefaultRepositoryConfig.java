f8e434de2a6150bf8025ec99778ff131a17f46fc
albertoornelasdev <albertoornelasdev@gmail.com>
2022-05-03 11:10:09 -0700

    Default Repository done

--- src/main/java/com/at/internship/schedule/config/DefaultRepositoryConfig.java
+++ src/main/java/com/at/internship/schedule/config/DefaultRepositoryConfig.java
@@ -2,27 +2,37 @@ package com.at.internship.schedule.config;
 
 import com.at.internship.schedule.repository.IAppointmentRepository;
 import com.at.internship.schedule.repository.IContactRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
-// @Configuration
+@Configuration
 public class DefaultRepositoryConfig {
+
+    @Value("${com.at.internship.schedule.repository.prefix}")
+    private static String DEFAULT_REPO;
 
     private final ApplicationContext context;
 
+
     public DefaultRepositoryConfig(ApplicationContext context) {
         this.context = context;
-        // this.context.getBean();
     }
 
     public IContactRepository contactRepository() {
-        //
-        return null;
+        if (DEFAULT_REPO.equals("csv")) {
+            return this.context.getBean(CsvRepositoryConfig.class).csvContactRepository();
+        }else{
+            return (IContactRepository) this.context.getBean(MockRepositoryConfig.BEAN_CONTACT_REPOSITORY);
+        }
     }
 
     public IAppointmentRepository appointmentRepository() {
-        //
-        return null;
+        if (DEFAULT_REPO.equals("csv")) {
+            return this.context.getBean(CsvRepositoryConfig.class).csvAppointmentRepository();
+        }else{
+            return (IAppointmentRepository) this.context.getBean(MockRepositoryConfig.BEAN_APPOINTMENT_REPOSITORY);
+        }
     }
 
 }
