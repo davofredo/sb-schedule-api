ea6756e0ce01d909d8b54612c7d1f62973e4265b
David Barredo <davofredo@gmail.com>
2022-05-06 13:40:08 -0500

    Contact & ContactPhone entities

--- src/main/java/com/at/internship/schedule/repository/IContactRepository.java
+++ src/main/java/com/at/internship/schedule/repository/IContactRepository.java
@@ -4,9 +4,6 @@ import com.at.internship.schedule.domain.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface IContactRepository extends JpaRepository<Contact, Integer> {
-
 }
