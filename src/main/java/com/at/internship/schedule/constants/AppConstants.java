bb23d71490b2fc1e762cfdabd67e87c0c7ffc29f
BrandMV <brandonmv2001@gmail.com>
2022-05-20 13:44:01 -0500

    inter12

--- src/main/java/com/at/internship/schedule/constants/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.at.internship.schedule.constants;
-
-import java.io.File;
-
-public class AppConstants {
-    public static final String SERIALIZATION_PATH = System.getProperty("user.dir") + File.separator;
-}
