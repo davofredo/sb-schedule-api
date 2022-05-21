45306a31803b96e037f57fad1530e38d638aac5e
Marco Antonio López Flores <mnmarcolf@gmail.com>
2022-05-20 23:15:41 -0500

    [ADD] add constants for codes and messages

---
+++ src/main/java/com/at/internship/schedule/constants/AppConstants.java
@@ -0,0 +1,13 @@
+package com.at.internship.schedule.constants;
+
+public class AppConstants {
+    public final static String STR_CODE_OK = "OK";
+    public final static String STR_MESSAGE_SUCCESS = "Success!";
+    public final static String STR_CODE_VALIDATION_FAILED = "VALIDATION_FAILED";
+    public final static String STR_MESSAGE_VALIDATION_FAILED = "There were validation errors";
+    public final static String STR_CODE_RECORD_NOT_FOUND= "RECORD_NOT_FOUND";
+    public final static String STR_MESSAGE_RECORD_NOT_FOUND = "Record not found";
+    public final static String STR_CODE_DB_CONSTRAINT_VIOLATION= "DB_CONSTRAINT_VIOLATION";
+    public final static String STR_MESSAGE_DB_CONSTRAINT_VIOLATION= "Database constraint violation";
+
+}
